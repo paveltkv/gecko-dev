d1b95e8adcb291ef42b21462eaf8685c3f2004f2
Pavel Tumakaev <p.tumakaev@omprussia.ru>
2021-03-10 21:44:18 +0300

    [sailfishos][embedlite] Remove APZCCallbackHelper::ApplyCallbackTransform()
    See upstream commits:
        dd4ac2120da54100aab173387d80f947196ae2a5
        f32f10a7197c9f44408be75dce22f3180941dae2

--- embedding/embedlite/embedshared/EmbedLiteViewChild.cpp
+++ embedding/embedlite/embedshared/EmbedLiteViewChild.cpp
@@ -38,6 +38,7 @@
 #include "mozilla/dom/Document.h"
 #include "mozilla/PresShell.h"
 #include "mozilla/layers/DoubleTapToZoom.h" // for CalculateRectToZoomTo
+#include "mozilla/layers/InputAPZContext.h" // for InputAPZContext
 #include "nsIFrame.h"                       // for nsIFrame
 #include "FrameLayerBuilder.h"              // for FrameLayerbuilder
 
@@ -790,10 +791,11 @@ EmbedLiteViewChild::InitEvent(WidgetGUIEvent& event, nsIntPoint* aPoint)
 
 mozilla::ipc::IPCResult EmbedLiteViewChild::RecvHandleDoubleTap(const LayoutDevicePoint &aPoint,
                                                                 const Modifiers &aModifiers,
-                                                                const ScrollableLayerGuid &aGuid)
+                                                                const ScrollableLayerGuid &aGuid,
+                                                                const uint64_t &aInputBlockId)
 {
   bool ok = false;
-  CSSPoint cssPoint = mHelper->ApplyPointTransform(aPoint, aGuid, &ok);
+  CSSPoint cssPoint = mHelper->ApplyPointTransform(aPoint, aGuid, aInputBlockId, &ok);
   NS_ENSURE_TRUE(ok, IPC_OK());
 
   nsIContent* content = nsLayoutUtils::FindContentFor(aGuid.mScrollId);
@@ -825,7 +827,8 @@ mozilla::ipc::IPCResult EmbedLiteViewChild::RecvHandleDoubleTap(const LayoutDevi
 
 mozilla::ipc::IPCResult EmbedLiteViewChild::RecvHandleSingleTap(const LayoutDevicePoint &aPoint,
                                                                 const Modifiers &aModifiers,
-                                                                const ScrollableLayerGuid &aGuid)
+                                                                const ScrollableLayerGuid &aGuid,
+                                                                const uint64_t &aInputBlockId)
 {
   if (mIMEComposing) {
     // If we are in the middle of compositing we must finish it, before it is too late.
@@ -839,7 +842,7 @@ mozilla::ipc::IPCResult EmbedLiteViewChild::RecvHandleSingleTap(const LayoutDevi
   }
 
   bool ok = false;
-  CSSPoint cssPoint = mHelper->ApplyPointTransform(aPoint, aGuid, &ok);
+  CSSPoint cssPoint = mHelper->ApplyPointTransform(aPoint, aGuid, aInputBlockId, &ok);
   NS_ENSURE_TRUE(ok, IPC_OK());
 
   if (sPostAZPCAsJson.singleTap) {
@@ -862,7 +865,7 @@ mozilla::ipc::IPCResult EmbedLiteViewChild::RecvHandleLongTap(const LayoutDevice
                                                               const uint64_t &aInputBlockId)
 {
   bool ok = false;
-  CSSPoint cssPoint = mHelper->ApplyPointTransform(aPoint, aGuid, &ok);
+  CSSPoint cssPoint = mHelper->ApplyPointTransform(aPoint, aGuid, aInputBlockId, &ok);
   NS_ENSURE_TRUE(ok, IPC_OK());
 
   if (sPostAZPCAsJson.longTap) {
@@ -1067,7 +1070,12 @@ mozilla::ipc::IPCResult EmbedLiteViewChild::RecvInputDataTouchEvent(const Scroll
   }
 
   UserActivity();
-  APZCCallbackHelper::ApplyCallbackTransform(localEvent, aGuid, mWidget->GetDefaultScale());
+
+  // Stash the guid in InputAPZContext so that when the visual-to-layout
+  // transform is applied to the event's coordinates, we use the right transform
+  // based on the scroll frame being targeted.
+  // The other values don't really matter.
+  InputAPZContext context(aGuid, aInputBlockId, aApzResponse);
 
   if (localEvent.mMessage == eTouchStart) {
     // CSS touch actions do not work yet. Thus, explicitly disabling in the code.
